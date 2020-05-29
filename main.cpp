057ed58e5214a97069c38e9decea0e3b116e0b71
Maty2002 <56909996+Maty2002@users.noreply.github.com>
2020-05-29 12:23:18 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<stdlib.h>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	string napis;
+	vector<string> tab;
+		
+	ifstream plik1;
+	ofstream plik2;
+	
+	string parm = argv[1];
+	string src;
+	
+	if(argc){
+		src="c:\\"+parm;
+	} else{
+		src="c:\\palindromy.txt";
+	}
+	
+	src="c:\\palindromy.txt";
+	
+	plik1.open("c:\\palindromy.txt");
+	
+	if(plik1.good()) {
+		 while(!plik1.eof()){
+	        getline(plik1, napis);
+			tab.push_back(napis);				
+	    };	
+	 };
+	 
+	cout<<"Content-Type: application/json;charset=UTF-8;Access-Control-Allow-Origin: *" << (char)13 << (char)10;
+	cout<<endl<<"[";
+	
+	for(int i=0;i<tab.size();i+=2){
+		cout<<"{\"imie"<<"\":\""<<tab[i]<<"\",\"subtitle\":\""<<tab[i+1]<<"\"}";
+		if(i<tab.size()-2)cout<<",";
+	};
+	
+	cout<<"]";
+	
+	plik1.close();
+	return 0;
+}
